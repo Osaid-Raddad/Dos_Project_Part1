f918c9cee82cdaa87b994c70c2a562d5f7a9cc69
Omar-Maher-Khatib <s12144231@stu.najah.edu>
2025-11-14 15:09:10 +0200

    Add index.js

--- src/order-service/index.js
+++ src/order-service/index.js
@@ -1,2 +1,35 @@
-// Order service implementation
-console.log('Order service started');
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('database.db');
+const axios = require("axios")
+const cors = require("cors");
+const app = express();
+const port = 3006;
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.post("/purch",async (req,res)=>{
+
+  const order = {
+    "id":req.body.id,
+    "orderCost":req.body.orderCost
+  };
+  try{
+    const response = await axios.post(`http://catalog-server:3005/order`,order);
+    console.log(response.data)
+    
+    res.send({message:"Send Request To Catalog"})
+  } catch(err){
+    console.log(err)
+    res.status(400).send({error:err})
+  }
+})
+
+app.get("/test",(req,res)=>{
+  res.send({Message:"Arrive"})
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
